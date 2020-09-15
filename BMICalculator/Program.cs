a86b29f98c85ffb2da8582d984e40a2222684670
Opilane TTHK <opilane@tthk.ee>
2020-09-15 12:20:36 +0300

    first commit

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            int age = Int32.Parse(Console.ReadLine());
+            
+
+            if (age < 20)
+            {
+                Console.WriteLine("You are too young to use the calculator");
+            } else if (age >= 60)
+            {
+                Console.WriteLine("You are too old to use the calculator");
+            } else
+            { 
+                double hight;
+                double Result;
+                Console.WriteLine("Enter your height");
+                hight = Double.Parse(Console.ReadLine());
+                Console.WriteLine($"your height is {hight} meters.");
+
+                double weight;
+                Console.WriteLine("Enter your weight");
+                weight = Double.Parse(Console.ReadLine());
+                Console.WriteLine($"your weight is {weight} kg.");
+                double height2 = hight * hight;
+                Result = weight / height2;
+
+                Console.WriteLine($"Your BMI is {Result} kg/m2.");
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
